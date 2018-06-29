d0f7176521d57771a56fa9ad252ad7f7e6a1e54d
kaconant <krissyconant@gmail.com>
2018-06-28 20:56:05 -0400

    cards flip

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
-import MemoryCard from './MemoryCard.js';
+import MemoryCard from './MemoryCard';
 import './App.css';
 
 
 class App extends Component {
   render() {
+    
+    let row1 = [];
+    let row2 = [];
+    let row3 = [];
+    let row4 = [];
+
+    for (let i=0; i<4; i++) {
+        row1.push(<MemoryCard />);
+        row2.push(<MemoryCard />);
+        row3.push(<MemoryCard />);
+        row4.push(<MemoryCard />);
+    }
+    
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Memory Game</h1>
           <h4 className="App-subtitle">Match cards to win!</h4>
         </header>
-        <body className="Card">
-          <div><MemoryCard></MemoryCard><MemoryCard></MemoryCard><MemoryCard></MemoryCard><MemoryCard></MemoryCard></div>
-          <div><MemoryCard></MemoryCard><MemoryCard></MemoryCard><MemoryCard></MemoryCard><MemoryCard></MemoryCard></div>
-          <div><MemoryCard></MemoryCard><MemoryCard></MemoryCard><MemoryCard></MemoryCard><MemoryCard></MemoryCard></div>
-          <div><MemoryCard></MemoryCard><MemoryCard></MemoryCard><MemoryCard></MemoryCard><MemoryCard></MemoryCard></div>
-        </body>
+
+          <div>
+            {row1}
+          </div>
+          <div>
+            {row2}
+          </div>
+          <div>
+            {row3}
+          </div>
+          <div>
+            {row4}
+          </div>
+          
       </div>
     );
   }
--- src/MemoryCard.js
+++ src/MemoryCard.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import './MemoryCard.css';
 
 class MemoryCard extends Component {
+    
+    constructor() {
+        super();
+        this.state = { isFlipped: false };
+    }
+
+    clickHandler() {
+        this.setState({ isFlipped: !this.state.isFlipped });
+    };
+
     render() {
+
+        let MemoryCardInnerClass = "MemoryCardInner";
+
+        if (this.state.isFlipped) {
+            MemoryCardInnerClass += " flipped";
+        };
+
+        console.log(MemoryCardInnerClass);
+
         return (
-            <div className="MemoryCard">
-                <img className="CardImage" src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" />
+            <div className="MemoryCard" onClick={ this.clickHandler.bind(this) }>
+                <div className={ MemoryCardInnerClass }>
+                    <div className="MemoryCardFront">
+                        ∆
+                    </div>
+                    <div className="MemoryCardBack">
+                        <img className="CardImage" src="https://www.digitalcrafts.com/img/DigitalCrafts-Logo-Wrench.png" alt="logo" />
+                    </div>
+                </div>
             </div>
         );
     }
