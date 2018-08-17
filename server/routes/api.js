65a2190e9ba3f76874e2a92f5d2d583f501e7a18
kaconant <krissyconant@gmail.com>
2018-08-17 12:52:22 -0400

    updated server into application

---
+++ server/routes/api.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+var axios = require('axios')
+
+/* GET github jobs listings in Atlanta */
+router.get('/jobs', function(req, res, next) {
+  axios.get('http://jobs.github.com/positions.json?location=atlanta')
+  	.then(({data})=>{
+  		res.json(data);
+  	}).catch((error)=>{
+      res.status(500).send(error)
+    });
+});
+
+/* GET individual github job listing in Atlanta */
+router.get('/jobs/:id', function(req, res, next) {
+  axios.get('http://jobs.github.com/positions/' + req.params.id + '.json')
+  	.then(({data})=>{
+      res.json(data);
+    }).catch((error)=>{
+      res.status(500).send(error)
+    });
+});
+
+module.exports = router;
