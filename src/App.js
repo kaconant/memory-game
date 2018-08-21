1dfb086afda00e3c000af08dd83369dbbc5a131c
kaconant <krissyconant@gmail.com>
2018-08-21 15:00:20 -0400

    removed memory game

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import React, {Component} from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
+import React, { Component } from 'react';
 import './App.css';
 import MemoryGame from './MemoryGame.js';
-import Jobs from './Jobs.js';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <div className='navbar'>
-                    <Link to='/memory'>Memory Game</Link>
-                    <Link to='/jobs'>Jobs in ATL</Link>
-                </div>
-                <Switch>
-                    <Route path='/memory' component={MemoryGame} />
-                    <Route path='/jobs' component={Jobs}/> 
-                </Switch>
-            </div>
+            <React.Fragment>
+                <MemoryGame/>
+            </React.Fragment>
         );
     }
 }
